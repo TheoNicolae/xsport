f6e75a41c96bdba83f41d680a4ec48796b9530b1
Theo Nicolae <theodor166@yahoo.com>
2018-08-04 13:39:35 +0300

    Initial commit

---
+++ src/main/java/com/example/demoapp/main/XsportApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoapp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsportApplication.class, args);
+	}
+}
